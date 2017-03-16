f0e2b0ec47ee1d646bcf6eaa8d81cfd715c4408d
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-15 15:03:11 -1000

    finally finished even_fib_nums.js

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,13 +5,35 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
+ var bin = [];
+
+ function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var a = 0;
+  var b = 1;
+  var temp = 0;
 
   // do your work here
 
+  while((a + b) <= maxFibValue){
+    console.log("this is what 'temp' is now: " + temp);
+    temp = a + b;
+    a = b;
+    b = temp;
+    bin.push(temp);
+    if(temp % 2 === 0){
+
+      sum += temp;
+    }
+
+  }
+    console.log("this is the result of 'sum': " + sum);
+    console.log("this is what 'temp' is now: " + temp);
+    console.log(bin);
   return sum;
 }
+console.log(_sumFibs);
+_sumFibs(4000000);
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
@@ -21,15 +43,19 @@ function _highestFibonacciNumber (maxFibValue){
 
 
   //do your work here
+  highest += bin[bin.length - 1];
+  console.log("this is the last number of the array" + highest);
 
   return highest;
-};
+}
+
+_highestFibonacciNumber();
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
-module.exports = {
+ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
